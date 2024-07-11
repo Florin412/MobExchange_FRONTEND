bac221cc4e64915d78dde9009564322c0091c28e
OrosRazvan <razvanros06@gmail.com>
2024-07-11 12:45:17 +0300

    SignIn Component

---
+++ exchange/src/components/SignIn/SIgnInForm.jsx
@@ -0,0 +1,35 @@
+import './SignInForm.css'
+import { useState } from 'react'
+
+function SignIn() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const SubmitForm = (e) => {
+        e.preventDefault()
+    }
+
+    return(
+        <>
+        <h1>Sign in</h1>
+        <form>
+            <div className="mb-3">
+              <label htmlFor="InputEmail" className="form-label-email">Email address</label>
+              <input onChange={(e) => {setEmail(e.target.value)}} type="email" className="form-control" id="InputEmail" aria-describedby="emailHelp"></input>
+             </div>
+          <div className="mb-3">
+              <label htmlFor="InputPassword" className="form-label-password">Password</label>
+              <input onChange={(e) => {setPassword(e.target.value)}} type="password" className="form-control" id="InputPassword" ></input>
+          </div>
+          <div className="mb-3-form-check">
+              <input type="checkbox" className="form-check-input" id="Check1"></input>
+              <label className="form-check-label" htmlFor="Check1">Remember me</label>
+          </div>
+          <button onClick={SubmitForm} type="submit" className="btn-primary">Submit</button>
+        </form>
+        </>
+    );
+}
+
+export default SignIn
