5fb42f851c98f353f61443369c1803e6a89940b0
raul406 <cioararaul70@yahoo.com>
2024-07-12 14:18:52 +0300

    imported SignUp component

--- exchange/src/App.jsx
+++ exchange/src/App.jsx
@@ -2,19 +2,20 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { useState } from "react";
 import "./App.css";
 import Navigation from "./components/Navbar/Navbar";
-import SignIn from './components/SignIn/SIgnInForm';
+import SignIn from "./components/SignIn/SIgnInForm";
+import SignUp from "./components/SignUp/SignUp";
 
 function App() {
   const [state, setState] = useState({
     user: { email: "Remus", password: "123456" },
     route: "signin",
-    isSignedIn: false
+    isSignedIn: false,
   });
 
   const onRouteChange = (newRoute) => {
     setState((prevState) => ({
       ...prevState,
-      route: newRoute
+      route: newRoute,
     }));
   };
 
@@ -26,15 +27,33 @@ function App() {
       ></Navigation>
 
       <Routes>
-        <Route path="/" element={<Navigate to={state.route === "home" ? "/home" : state.route === "signin" ? "/signin" : "/register"}></Navigate>}></Route>
-        <Route path="/home" element={<p>here you are on the home page</p>}></Route>
+        <Route
+          path="/"
+          element={
+            <Navigate
+              to={
+                state.route === "home"
+                  ? "/home"
+                  : state.route === "signin"
+                  ? "/signin"
+                  : "/register"
+              }
+            ></Navigate>
+          }
+        ></Route>
+        <Route
+          path="/home"
+          element={<p>here you are on the home page</p>}
+        ></Route>
         <Route path="/signin" element={<SignIn></SignIn>}></Route>
-        <Route path="/register" element={<SingUp></SingUp>}></Route>
-        <Route path="*" element={<Navigate to={"/register"}></Navigate>}></Route>
+        <Route path="/register" element={<SignUp></SignUp>}></Route>
+        <Route
+          path="*"
+          element={<Navigate to={"/register"}></Navigate>}
+        ></Route>
       </Routes>
-      </div>
+    </div>
   );
 }
 
 export default App;
-
