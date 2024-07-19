f5f810bfffb62ff987d18a41a800bb2a42e0f8dc
OrosRazvan <razvanros06@gmail.com>
2024-07-19 13:59:58 +0300

    Navbar and Footer Design

--- exchange/src/components/Navbar/Navbar.jsx
+++ exchange/src/components/Navbar/Navbar.jsx
@@ -1,24 +1,25 @@
 import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // eslint-disable-next-line react/prop-types
 const Navigation = ({ isSignedIn, onSignedInChange, onRouteChange }) => {
   if (isSignedIn) {
-    // Here is the template for when the user is logged in.
+    // Template for when the user is logged in
     return (
-      <nav className="navbar navbar-expand-lg bg-body-tertiary" >
-        <div className="container-fluid" >
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
           {/* Logo */}
           <Link
-            className="navbar-brand"
+            className="navbar-brand ms-auto"
             to="/home"
             onClick={() => {
               onSignedInChange(true);
               onRouteChange("home");
             }}
-            style={{ marginLeft: '50px', fontSize: '25px' }}
           >
-           <span style={{ color: 'white' }}>Mobi</span>
-           <span style={{ color: '#FFD824' }}>Exchange</span>
+            <h1 className="fs-1 fs-md-3 mb-0">
+              Mobi<span className="text-warning">Exchange</span>
+            </h1>
           </Link>
 
           {/* Button appears for mobile devices */}
@@ -49,8 +50,19 @@ const Navigation = ({ isSignedIn, onSignedInChange, onRouteChange }) => {
                     onSignedInChange(false);
                     onRouteChange("signin");
                   }}
+                  style={{
+                    backgroundColor: '#FFD824',
+                    color: 'black',
+                    border: '1px solid black',
+                    padding: '8px 16px',
+                    marginRight: '60px',
+                    fontSize: '18px',
+                    fontFamily: 'Poppins',
+                    borderRadius: '30px',
+                    boxShadow: 'rgba(0, 0, 0, 0.2) 0px 5px 15px',
+                  }}
                 >
-                  SignOut
+                  Sign Out
                 </Link>
               </li>
             </ul>
@@ -59,98 +71,94 @@ const Navigation = ({ isSignedIn, onSignedInChange, onRouteChange }) => {
       </nav>
     );
   } else {
-    // Here is the template for when the user is NOT logged in.
+    // Template for when the user is NOT logged in
     return (
-      <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary" >
-          <div className="container-fluid" style={{ backgroundColor: "#28292D", margin: "-6px 0px", height: "70px"}}>
-            {/* Logo */}
-            <Link
-              className="navbar-brand"
-              to="/home"
-              onClick={() => {
-                onSignedInChange(true);
-                onRouteChange("home");
-              }}
-              style={{ marginLeft: '50px', fontSize: '35px', fontFamily: "Poppins" }}
-            >
-              <span style={{ color: 'white' }}>Mobi</span>
-              <span style={{ color: '#FFD824' }}>Exchange</span>
-            </Link>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          {/* Logo */}
+          <Link
+            className="navbar-brand ms-auto"
+            to="/home"
+            onClick={() => {
+              onSignedInChange(true);
+              onRouteChange("home");
+            }}
+          >
+            <h1 className="fs-1 fs-md-3 mb-0">
+              Mobi<span className="text-warning">Exchange</span>
+            </h1>
+          </Link>
 
-            {/* Button appears for mobile devices */}
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
+          {/* Button appears for mobile devices */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-            {/* The actual links from navbar */}
-            <div
-              className="collapse navbar-collapse justify-content-end"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav">
-                {/* SignIn Link */}
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to="/signin"
-                    onClick={() => {
-                      onSignedInChange(false);
-                      onRouteChange("signin");
-                    }}
-                    style={{
-                      backgroundColor: 'white',
-                      color: 'black',
-                      border: '1px solid black',
-                      padding: '8px 16px', 
-                      marginRight: '10px',
-                      fontFamily: 'Poppins',
-                      fontSize: '18px',  
-                      borderRadius: '30px',
-                      boxShadow: 'rgba(0, 0, 0, 0.2) 0px 5px 15px',
-                    }}
-                  >
-                    Sign In
-                  </Link>
-                </li>
+          {/* The actual links from navbar */}
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav">
+              {/* SignIn Link */}
+              <li className="nav-item">
+                <Link
+                  className="nav-link d-block d-sm-inline text-end mx-2"
+                  to="/signin"
+                  onClick={() => {
+                    onSignedInChange(false);
+                    onRouteChange("signin");
+                  }}
+                  style={{
+                    backgroundColor: 'white',
+                    color: 'black',
+                    border: '1px solid black',
+                    padding: '8px 16px',
+                    fontFamily: 'Poppins',
+                    fontSize: '15px',
+                    borderRadius: '30px',
+                    boxShadow: 'rgba(0, 0, 0, 0.2) 0px 5px 15px',
+                  }}
+                >
+                  Sign In
+                </Link>
+              </li>
 
-                {/* Register Link */}
-                <li className="nav-item">
-                  <Link
-                    className="nav-link"
-                    to="/signup"
-                    onClick={() => {
-                      onSignedInChange(false);
-                      onRouteChange("signup");
-                    }}
-                    style={{
-                      backgroundColor: '#FFD824',
-                      color: 'black',
-                      border: '1px solid black',
-                      padding: '8px 16px',  
-                      marginRight: '60px',
-                      fontSize: '18px',  
-                      fontFamily: 'Poppins',
-                      borderRadius: '30px',
-                      boxShadow: 'rgba(0, 0, 0, 0.2) 0px 5px 15px',
-                    }}
-                  >
-                    Sign Up
-                  </Link>
-                </li>
-              </ul>
-            </div>
+              {/* Register Link */}
+              <li className="nav-item">
+                <Link
+                  className="nav-link d-block d-sm-inline text-end mx-2"
+                  to="/signup"
+                  onClick={() => {
+                    onSignedInChange(false);
+                    onRouteChange("signup");
+                  }}
+                  style={{
+                    backgroundColor: '#FFD824',
+                    color: 'black',
+                    border: '1px solid black',
+                    padding: '8px 16px',
+                    fontFamily: 'Poppins',
+                    fontSize: '15px',
+                    borderRadius: '30px',
+                    boxShadow: 'rgba(0, 0, 0, 0.2) 0px 5px 15px',
+                  }}
+                >
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
     );
   }
 };
--- exchange/src/components/footer/Footer.jsx
+++ exchange/src/components/footer/Footer.jsx
@@ -1,36 +1,90 @@
-import "./Footer.css";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Footer = () => {
+  const [activeButton, setActiveButton] = useState(null);
+
+  const buttonStyle = {
+    backgroundColor: 'transparent',
+    border: 'none',
+    color: '#fff',
+  };
+
+  const activeButtonStyle = {
+    backgroundColor: 'transparent',
+    border: 'none',
+    color: '#ffd824', // Highlight active button with a different color
+  };
+
+  const handleMouseDown = (button) => {
+    setActiveButton(button);
+  };
+
+  const handleMouseUp = () => {
+    setActiveButton(null);
+  };
+
   return (
-    <div className="footer-div">
-      <h1>
-        Mobi<span className="footer-span">Exchange</span>
-      </h1>
-      <div className="footer-links">
-        <button className="footer-button">
-          <h3 className="footer-text">
-            <Link to="/about" className="link-footer">
-              About
-            </Link>
-          </h3>
-        </button>
-        <button className="footer-button">
-          <h3 className="footer-text">
-            <Link to="/privacy" className="link-footer">
-              Privacy
-            </Link>
-          </h3>
-        </button>
-        <button className="footer-button">
-          <h3 className="footer-text">
-            <Link to="/cookie" className="link-footer">
-              Cookie
-            </Link>
-          </h3>
-        </button>
+    <footer className="container-fluid px-4 py-3 bg-dark text-light">
+      <div className="row align-items-center" style={{ height: '70px' }}>
+        {/* Logo Section */}
+        <div className="col-12 col-md-6 text-center text-md-start mb-3 mb-md-0">
+          <h1 className="fs-1 fs-md-3 mb-0">
+            Mobi<span className="text-warning">Exchange</span>
+          </h1>
+        </div>
+        {/* Links Section */}
+        <div className="col-12 col-md-6 d-flex flex-column flex-md-row justify-content-center justify-content-md-end align-items-center gap-3">
+          <button
+            className="btn"
+            style={activeButton === "about" ? activeButtonStyle : buttonStyle}
+            onMouseDown={() => handleMouseDown("about")}
+            onMouseUp={handleMouseUp}
+          >
+            <h3 className="m-0 fs-3 fs-md-5">
+              <Link to="/about" className="text-white text-decoration-none">
+                About
+              </Link>
+            </h3>
+          </button>
+          <button
+            className="btn"
+            style={activeButton === "privacy" ? activeButtonStyle : buttonStyle}
+            onMouseDown={() => handleMouseDown("privacy")}
+            onMouseUp={handleMouseUp}
+          >
+            <h3 className="m-0 fs-3 fs-md-5">
+              <Link to="/privacy" className="text-white text-decoration-none">
+                Privacy
+              </Link>
+            </h3>
+          </button>
+          <button
+            className="btn"
+            style={activeButton === "cookie" ? activeButtonStyle : buttonStyle}
+            onMouseDown={() => handleMouseDown("cookie")}
+            onMouseUp={handleMouseUp}
+          >
+            <h3 className="m-0 fs-3 fs-md-5">
+              <Link to="/cookie" className="text-white text-decoration-none">
+                Cookie
+              </Link>
+            </h3>
+          </button>
+        </div>
       </div>
-    </div>
+      {/* Extend background color on mobile */}
+      <div className="d-block d-md-none" style={{
+        position: 'relative',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        minHeight: '70px', // Ensure it matches the footer height
+        backgroundColor: '#28292D', // Match the background color
+        zIndex: -1, // Ensure it doesn't cover footer content
+      }}></div>
+    </footer>
   );
 };
 
