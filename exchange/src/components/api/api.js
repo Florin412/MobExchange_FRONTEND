c9f8d85b0e8b0a9de0b5778de588edcaf59c7d30
raul406 <cioararaul70@yahoo.com>
2024-07-22 14:17:09 +0300

    home with our api

--- exchange/src/components/api/api.js
+++
@@ -1,5 +0,0 @@
-export const url = `https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/`;
-
-export const api_key = `ddf8050f97a99073d99d4dcf`;
-
-export let base_curr = "EUR";
--- exchange/src/components/home/Home.jsx
+++ exchange/src/components/home/Home.jsx
@@ -7,28 +7,40 @@ function Home() {
   const [exchangeRatesState, setExchangeRatesState] = useState("");
   const [currencyFrom, setCurrencyFrom] = useState("USD");
   const [availableCurrencies, setAvailableCurrencies] = useState([]);
-  const [allCurrenciesAvailable, setAllCurrenciesAvailable] = useState([]);  
+  const [allCurrenciesAvailable, setAllCurrenciesAvailable] = useState([]);
   const [tableInfo, setTableInfo] = useState([]);
-
+  const [numberToConvert, setNumberToConvert] = useState("");
+  const [currencyFinal, setCurrencyFinal] = useState("USD");
+  const [currencyTo, setCurrencyTo] = useState("");
+  const [clickAddCurrency, setClickAddCurrency] = useState(false);
+  const [allCurrencies, setAllCurrencies] = useState(allCurrenciesAvailable);
+  const [liveExchange, setLiveExchange] = useState();
+  const [tableInfoState, setTableInfoState] = useState(tableInfo);
 
   useEffect(() => {
     const fetchExchangeApi = async () => {
-      const api_key = "ddf8050f97a99073d99d4dcf";
-      const url = `https://v6.exchangerate-api.com/v6/${api_key}/latest/${currencyFrom}`;
+      const url = `http://192.168.170.144:8080/exchange-rates/latest/${currencyFrom}`;
+
       try {
         const response = await axios.get(url);
-        if (response.data.result === "success") {
-          setExchangeRatesState(response.data.conversion_rates);
-          setAvailableCurrencies(Object.keys(response.data.conversion_rates));
+        if (response.status === 200) {
+          const rates = response.data.rates;
+          const baseCurrency = response.data.base;
 
-          const currencies = Object.entries(response.data.conversion_rates).map(
+          setExchangeRatesState(response.data.rates);
+
+          setAvailableCurrencies([baseCurrency, ...Object.keys(rates)]);
+
+          const currencies = Object.entries(response.data.rates).map(
             ([currency, rate], index) => [index + 1, currency, rate]
           );
           setAllCurrenciesAvailable(currencies);
 
-          const filteredCurrencies = currencies.filter(
-            ([_, currency]) => ["RON", "EUR", "USD", "GBP"].includes(currency)
-          ).map(([_, currency, rate], index) => [index + 1, currency, rate]);
+          const filteredCurrencies = currencies
+            .filter(([_, currency]) =>
+              ["RON", "EUR", "USD", "GBP"].includes(currency)
+            )
+            .map(([_, currency, rate], index) => [index + 1, currency, rate]);
 
           setTableInfoState(filteredCurrencies);
         } else {
@@ -44,51 +56,17 @@ function Home() {
     fetchExchangeApi();
   }, [currencyFrom]);
 
-
-
-  // const exchangeRates = {
-  //   USD: { RON: 4.1, EUR: 0.85, GBP: 0.75, USD: 1 },
-  //   RON: { USD: 0.24, EUR: 0.21, GBP: 0.18, RON: 1 },
-  //   EUR: { USD: 1.18, RON: 4.88, GBP: 0.88, EUR: 1 },
-  //   GBP: { USD: 1.34, RON: 5.56, EUR: 1.14, GBP: 1 },
-  // };
-
-  // const tableInfo = [
-  //   [1, "RON", 1, "+0.075%"],
-  //   [2, "USD", 0.24, "+0.075%"],
-  //   [3, "EUR", 0.21, "-0.055%"],
-  //   [4, "GBP", 0.75, "+0.002%"],
-  // ];
-
-  // const allCurrenciesAvailable = [
-  //   [1, "RON", 1, "+0.075%"],
-  //   [2, "USD", 0.24, "+0.075%"],
-  //   [3, "EUR", 0.21, "-0.055%"],
-  //   [4, "GBP", 0.75, "+0.002%"],
-  //   [5, "TEST", 1.11, "-0.001%"],
-  // ];
-
-  //let availableCurrencies = ["RON", "USD", "EUR", "GBP", "TEST"];
-
   let titlesTables = ["#", "CurrName", "Price", "Percent"];
 
   let fromCurr = useRef(null);
 
-
-
-  const [numberToConvert, setNumberToConvert] = useState("");
-  const [currencyFinal, setCurrencyFinal] = useState("USD");
-  const [currencyTo, setCurrencyTo] = useState("");
-  const [clickAddCurrency, setClickAddCurrency] = useState(false);
-  const [allCurrencies, setAllCurrencies] = useState(allCurrenciesAvailable);
-  const [liveExchange, setLiveExchange] = useState();
-  const [tableInfoState, setTableInfoState] = useState(tableInfo);
-
-  const availableCurrenciesFunction = () =>{
-    console.log('available curr')
-  }
-
   const handleChange = (e) => {
+    console.log(
+      setTableInfoState,
+      setAllCurrencies,
+      setTableInfo,
+      liveExchange
+    );
     const value = e.target.value;
     try {
       if (isNaN(Number(value))) {
@@ -104,57 +82,38 @@ function Home() {
   //convertHandler with api data
   const convertHandler = () => {
     try {
-      console.log(`The conversion is from ${currencyFrom} to ${currencyFinal}`)
-     
-      let result = Object.keys(exchangeRatesState).map(key => {
-        return { currency: key, rate: exchangeRatesState[key] };});
+      console.log(`The conversion is from ${currencyFrom} to ${currencyFinal}`);
 
-      let exchangeRateTo = result.filter(item => item.currency.toLowerCase() === currencyFinal.toLowerCase())
-      console.log(exchangeRateTo)
+      let result = Object.keys(exchangeRatesState).map((key) => {
+        return { currency: key, rate: exchangeRatesState[key] };
+      });
 
-      if(exchangeRateTo === undefined){
-        throw new Error("We dont have that currency status")
+      let exchangeRateTo = result.filter(
+        (item) => item.currency.toLowerCase() === currencyFinal.toLowerCase()
+      );
+      console.log(exchangeRateTo);
+
+      if (exchangeRateTo === undefined) {
+        throw new Error("We dont have that currency status");
       }
 
-      let convertedValue = (Number(numberToConvert) * exchangeRateTo.map((item)=> item.rate)).toFixed(2);
-      console.log(convertedValue)
-      setCurrencyTo(convertedValue)
-     
+      let convertedValue = (
+        Number(numberToConvert) * exchangeRateTo.map((item) => item.rate)
+      ).toFixed(2);
+      console.log(convertedValue);
+      setCurrencyTo(convertedValue);
     } catch (err) {
       console.error(err);
     }
   };
 
-
-
-  // converthandler with dummy data
-  // const convertHandler = () => {
-  //   try {
-  //     console.log(exchangeRatesState)
-  //     console.log(
-  //       `Converting from ${currencyFrom} to ${currencyFinal} with amount ${numberToConvert}`
-  //     );
-  //     const rate = exchangeRates[currencyFrom]?.[currencyFinal];
-  //     console.log(rate + " rate");
-  //     if (rate === undefined) {
-  //       throw new Error("Conversion rate not found");
-  //     }
-  //     const convertedValue = (Number(numberToConvert) * rate).toFixed(2);
-  //     console.log(convertedValue);
-  //     setCurrencyTo(convertedValue);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
-
   const handleCurrencyFromChange = (e) => {
     setCurrencyFrom(e.target.value);
+    console.log(e.target.value);
   };
-
 
   const handleCurrencyToChange = (e) => {
     setCurrencyFinal(e.target.value);
-  
   };
 
   const handleClickAddCurrency = (e) => {
@@ -165,9 +124,11 @@ function Home() {
 
   const handleLiveExchange = (e) => {
     const value = e.target.value;
-   
+
     setLiveExchange(value);
-    const [filteredInfo] = allCurrenciesAvailable.filter((item) => item[1] === value);
+    const [filteredInfo] = allCurrenciesAvailable.filter(
+      (item) => item[1] === value
+    );
     console.log(filteredInfo);
 
     const isAlreadyListed = tableInfoState.some((item) => item[1] === value);
@@ -175,6 +136,7 @@ function Home() {
 
     if (isAlreadyListed === false) {
       const [idTemp, currency, price, percent] = filteredInfo;
+      console.log(idTemp, allCurrencies);
       let id = tableInfoState.length + 1;
       tableInfo.push[(currency, price, percent)];
       setTableInfoState((prevTableInfo) => [
@@ -202,14 +164,12 @@ function Home() {
               className="labelFromSelect"
               value={currencyFrom}
               onChange={handleCurrencyFromChange}
-            
             >
               {availableCurrencies.map((item, index) => (
                 <option key={index} value={item}>
                   {item}
                 </option>
               ))}
-              
             </select>
           </div>
 
@@ -237,7 +197,7 @@ function Home() {
               value={currencyFinal}
               onChange={handleCurrencyToChange}
             >
-               {availableCurrencies.map((item, index) => (
+              {availableCurrencies.map((item, index) => (
                 <option key={index} value={item}>
                   {item}
                 </option>
@@ -257,7 +217,7 @@ function Home() {
       </div>
 
       <div className="second-header-div">
-        <h2 className="second-header">Live Exchange Value</h2>
+        <h2 className="second-header">Live Exchange Value ${currencyFrom}</h2>
       </div>
 
       <div className="mainDiv tableDiv">
