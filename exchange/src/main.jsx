32c1e912c861a5dfcf139b46eddbf91bd18ffea9
Lupau Remus <lupau.remus15@gmail.com>
2025-03-17 20:16:15 +0200

    component for scroll up in page, added

--- exchange/src/main.jsx
+++ exchange/src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         v7_relativeSplatPath: true
       }}
     >
+      <ScrollToTop></ScrollToTop>
       <App />
     </BrowserRouter>
   </React.StrictMode>
